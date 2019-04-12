08f0e9693704739f524ce6419599d9ced3d21ebf
MartinLindblad <martin.lindblad1@gmail.com>
2019-04-12 11:28:10 +0200

    no message

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-anime({
-  targets: '.morphing-demo .polymorph',
-  points: [
-    { value: [
-      '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
-      '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369']
-    }
-  ],
-  easing: 'easeOutQuad',
-  duration: 2000,
-  loop: true
-});                                                                       
+$(document).ready(function() {
+
+  let btn = $('#click');
+  let input = $('.input-container');
+  let overlay = $('#svg-morph');
+  console.log(overlay);
+  
+  let ani = anime({
+    targets: '#svg-morph',
+    d:[
+      {
+        value: 'M0,0H1180c102.333,0,270.2,2.115,366.666,0,209,2,377.669,0,377.669,0H0Z'
+      },{
+        value: 'M0,0V1082.334H1070c102.333,2,252.538,2.115,349,0,209,2,505.331,0,505.331,0V0Z'
+      }
+    ],
+    easing: 'easeinOutQuint',
+    duration: 1500,
+    loop: false,
+    // autoplay: false,
+    
+  });
+  console.log(anime());
+  
+}); // Ready
